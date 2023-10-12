825c8b87b63806c2d3ea5fd119116e9ffca891f0
Anmol Thakur <anmolll.thakurrr@gmail.com>
2023-10-12 19:31:55 +0530

    first commit

---
+++ src/Components/index.js
@@ -0,0 +1,11 @@
+import InputBox from "./InputBox";
+
+export {InputBox}
+
+
+
+
+//this way is used in big organizations for optimization
+//we will import every component in this file and also export it.
+//So it will help in optimization as instead of importing or rending every single component
+//this file can be used to import and export multiple components
